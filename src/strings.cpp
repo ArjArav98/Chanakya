f7171eb141acb92d1ef872b9090f1e6abbfbbf10
Arjun Aravind <arjun.aravind1998@gmail.com>
2019-04-17 16:06:32 +0530

    Add operator overloads to String class.
    Added operator overloads to the string class for the =, << and the >> operators.

--- src/strings.cpp
+++ src/strings.cpp
@@ -7,18 +7,64 @@ using namespace std;
 class String {
 
 	char* string;
+	bool allocated;
 
 public:
 	String() {
 		string = NULL;
+		allocated = false;
 	}
 
 	String(const char* passedString) {
-		string = passedString;
+		string = (char*) passedString;
+		allocated = false;
 	}
 
+	/* PUBLIC METHODS */
+
 	int length() {
-		
+		int count=0;
+		for(count=0; string[count]!='\0'; count++);
+		return count;
+	}
+
+	/* OPERATOR OVERLOADING */
+
+	void operator =(const char* passedString) {
+		eraseString();
+		string = (char*) passedString;
+	}
+
+	friend ostream &operator <<(ostream &out, String &str);
+	friend istream &operator >>(istream &in, String &str);
+
+	/* PRIVATE METHODS */
+
+	void eraseString() {
+		if(allocated == true) {
+			delete string;
+			allocated = false;
+		}
 	}
 
 };
+
+ostream &operator <<(ostream &out, String &str) {
+
+	for(int iter=0; iter<str.length(); iter++) {
+		out<<str.string[iter];
+	}
+
+	return out;
+
+}
+
+int main() {
+
+	String hello = "hello";
+	cout<<hello.length();
+
+	cout<<hello;
+
+	return 0;
+}
