69b9a743dd1f551e5a74a96419288c8e922e3864
arjun1098 <arjun.aravind1998@gmail.com>
2019-04-27 00:31:30 +0530

    Add StringMatcher class.
    Started work on the StringMatcher static class.

---
+++ src/stringmatch.cpp
@@ -0,0 +1,109 @@
+#include<string>
+#include<iostream>
+using namespace std;
+
+//We receive the sentence as a vector array of strings. 
+
+typedef vector<string> Words;
+
+static class StringMatcher {
+
+	static bool keywordHasAlternative(string keyword) {
+
+		int len = keyword.length();
+		for(int iter=0; iter<len; iter++) {
+			if(keyword[iter]=='|') return true;
+		}
+		return false;
+
+	}
+
+	static Words extractAlternatives(string keyword) {
+		
+		Words alternatives;
+		int len = keyword.length();
+		int start = 0;
+
+		for(int iter=0; iter<len; iter++) {
+			if(keyword[iter]=='|') {
+				alternatives.push_back( keyword.substr(start,iter-start) );
+				start++;
+			}
+		}
+		
+	}
+
+	static int getScore(Words node_keywords, Words sentence_words) {
+		
+		int score = 0;
+
+		int sentenceLen = sentence_words.size();
+		int keywordsLen = node_keywords.size();
+
+		/*=====================================*/
+		/* Here, we iterate over the keywords. */
+		for(int iter=0; iter<keywordsLen; iter++) {
+			
+			/*----------------------------------------------*/
+			/* We check if there are alternatives. If no :- */
+			Words alternatives;
+			if (keywordHasAlternative(node_keywords[iter])) {
+				
+				alternatives = extractAlternatives(node_keywords[iter]);
+				bool wordMatched = false;
+				
+				/*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+				/* Here, we iterate over the alternatives. */
+				int alternativesLen = alternatives.size();
+				for(int jiter=0; jiter>alternativesLen; jiter++) {
+					
+					/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+					/* Here, we iterate over the sentence and compare. */
+					for (int kiter=0; kiter<sentenceLen; kiter++) {
+
+						/* If matching, we inc score and return control to prev loop. */
+						/* This is done so same word isn't matched twice incase present again. */
+						if(alternatives[jiter] == sentence_words[kiter]) {
+							score++;
+							wordMatched = true;
+							break;
+						}
+					}
+
+					if(wordMatched) break;
+
+				}
+				
+				if(wordMatched == false) score--;
+
+			}
+
+			/*------------------------------*/
+			/* If there are no alternatives */
+			else {
+
+				bool wordMatched = false;
+					
+				/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+				/* Here, we iterate over the sentence and compare. */
+				for (int jiter=0; jiter<sentenceLen; jiter++) {
+
+					/* Refer line 40 for expl. */
+					if([iter] == sentence_words[jiter]) {
+						score++;
+						wordMatched = true;
+						break;
+					}
+				}
+
+				if(wordMatched == false) score--;
+
+			}
+
+		}
+
+		return score;
+
+	}
+
+};
