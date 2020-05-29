3fe51369a9b68520dc55ba90ea3c0bd351f9dbc7
Arjun Aravind <arjun.aravind1998@gmail.com>
2020-05-30 03:42:40 +0530

    Add more functions to the JsonValidator class.

--- include/json/test.cpp
+++ include/json/test.cpp
@@ -43,18 +43,67 @@ class JsonValidator {
 		return true;
 	}
 
-    // bool stringsAreInQuotes(string filename) {
+    /* We extract each string and see if it is formatted properly. */
+    bool stringsAreFormattedProperly(string filename) {
 
-    //     ifstream jsonFile(filename);
-    //     char currentChar, previousChar;
+        ifstream jsonFile(filename);
+        int start, length = 0;
+        bool stringHasStarted = false;
+        bool quotesIsNotOpen = true;
 
-    //     while(true) {
+        while(true) {
+            char character;
+            jsonFile >> character;
 
-    //     }
+            if(jsonFile.eof()) {
+                if(stringHasStarted && stringNotEnclosedByQuotes(filename, start-1, length))
+                    return false;
+                break;
+            }
 
-    // }
+            if(character == '\\' && stringHasStarted) { /* Escaping characters. */
+                length+=2;
+                jsonFile >> character;
+                continue;
+            }
+            if( character == '"' || character == ',' ||
+                character == ':' || character == '[' ||
+                character == ']' || character == '{' || 
+                character == '}' ) { /* Structural characters */
 
-    public:
+                if(stringHasStarted) {
+                    stringHasStarted = false;
+                    if(stringNotEnclosedByQuotes(filename, start-1, length)) return false;
+                    length = 0;
+                }
+            }
+            else { /* Literally anything else. */
+                if(!stringHasStarted) {
+                    stringHasStarted = true;
+                    start = jsonFile.tellg();
+                }
+                length++;
+            }
+        }
+
+        return true;
+    }
+
+    /* We check if the given range is preceded by quotes. */
+    bool stringNotEnclosedByQuotes(string filename, int start, int length) {
+        ifstream file(filename);
+        char character;
+
+        file.seekg(start-1);
+        if(file.get() != 34) return true;
+
+        for(int iter=start; iter<(start+length+1); iter++) file >> character;
+        if(character != '"') return true;
+
+        return false;
+    }
+
+    private:
     bool isANumber(string input) {
         int length = input.length();
         for(int iter=0; iter<length; iter++) {
@@ -92,7 +141,7 @@ class JsonValidator {
 
 int main() {
 	JsonValidator json;
-    cout<<json.extractFromFile("test.json", 120, 20);
+    cout<<json.stringsAreFormattedProperly("test.json")<<endl;
 
     return 0;
 
